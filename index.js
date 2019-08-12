73708c60037adacf2262cc38f30efa8b24709ced
Edemone <edem.agbenyo@gmail.com>
2019-08-12 11:20:45 +0000

    Calculate price of different book in set

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-function priceCalculator(books){
-    if(books.length==2){
-        for (let i = 0; i < books.length; i++) {
-            
-        }
-    }else if(books.length==1){
-        return 8 * books[0];
+function priceCalculator(books) {
+  if (books.length > 1) {
+    let discount = 1;
+    switch (books.length) {
+      case 2:
+        discount = 0.05;
+        break;
+      case 3:
+        discount = 0.1;
+        break;
+      case 4:
+        discount = 0.2;
+        break;
+      case 5:
+        discount = 0.25;
+        break;
+      default:
+        discount = 1;
     }
-    return null;
+    let price = 0;
+    let min = Math.min(...books);
+    if (books.every(i => i == min)) {
+      price += 8 * (1 - discount) * min * books.length;
+    }
+    for (let i = 0; i < books.length; i++) {}
+    return price;
+  } else if (books.length == 1) {
+    return 8 * books[0];
+  }
+  return 0;
 }
 
 module.exports = priceCalculator;
--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -2,8 +2,8 @@ const priceCalculator = require('../index');
 
 describe('Book Price Calculator',function(){
 
-    it('should return null for 0 book',function(){
-        expect(priceCalculator([])).toBe(null)
+    it('should return 0 for 0 book',function(){
+        expect(priceCalculator([])).toBe(0)
     })
     describe('Same book',function(){
         it('should calculate price of 1 book',function(){
@@ -20,6 +20,31 @@ describe('Book Price Calculator',function(){
         })
         it('should calculate price of 5 books',function(){
             expect(priceCalculator([5])).toBe(40)
+        })
+    });
+
+    describe('Different book that form a set',function(){
+        it('should calculate price of 2 books',function(){
+            expect(priceCalculator([1,1])).toEqual(15.2);
+            expect(priceCalculator([2,2])).toEqual(30.4);
+            expect(priceCalculator([5,5])).toEqual(76.0);
+        })
+        it('should calculate price of 3 books',function(){
+            expect(priceCalculator([1,1,1])).toEqual(21.6);
+        })
+        it('should calculate price of 4 books',function(){
+            expect(priceCalculator([1,1,1,1])).toEqual(25.6);
+        })
+        it('should calculate price of 5 books',function(){
+            expect(priceCalculator([1,1,1,1,1])).toEqual(30);
+            expect(priceCalculator([2,2,2,2,2])).toEqual(60);
+            expect(priceCalculator([5,5,5,5,5])).toEqual(150);
+        })
+    });
+
+    describe('Different book that form a set with additional books',function(){
+        it('should calculate price of 2 books',function(){
+            // expect(priceCalculator([1,2])).toEqual(23.2);
         })
     })
     
