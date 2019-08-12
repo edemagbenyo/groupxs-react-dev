23ca05e232e3cc109295a71e9cf85c1af730165f
Edemone <edem.agbenyo@gmail.com>
2019-08-12 12:02:23 +0000

    Minor changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function priceCalculator(books) {
+    let price = 8;
     if (books.length > 1) {
       let discount = 1;
+      //Get the discount based on the number of books
       switch (books.length) {
         case 2:
           discount = 0.05;
@@ -17,22 +19,26 @@ function priceCalculator(books) {
         default:
           discount = 1;
       }
-      let price = 0;
+      //Get the minimum number of book in the set
       let min = Math.min(...books);
       if (books.every(i => i == min)) {
-        return 8 * (1 - discount) * min * books.length;
+        return price * (1 - discount) * min * books.length;
       }else{
-        let sameBookPrice = (8 * (1 - discount) * min * books.length);
-        
+        //Calculate the price of all different book in a set
+        let sameBookPrice = (price * (1 - discount) * min * books.length);
+
+        //Remove the book with the lowest number from the book set
         books.splice(books.indexOf(min),1);
 
+        //Reduce the number of the book by the number 
         let reducedBooks = books.map(i=>i-min);
 
         return sameBookPrice + priceCalculator(reducedBooks);
       }
       
     } else if (books.length == 1) {
-      return 8 * books[0];
+        //Get the price of one book sample
+      return price * books[0];
     }
     return 0;
   }
